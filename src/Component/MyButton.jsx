0ac487d51f3a8125f47f6ffab1b36e9ca46f8a86
Doha-AboElkasem <Kassemhossam878@gmail.com>
2025-09-13 14:03:00 +0200

    NavBar

--- src/Component/MyButton.jsx
+++ src/Component/MyButton.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+// import React from "react";
 
-const MyButton = ({ children, disabled, bgColor, ...restProps }) => {
+// const MyButton = ({ children, disabled, bgColor, ...restProps }) => {
+//   return (
+//     <button
+//       className={`bg-[${bgColor}] hover:brightness-110 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-0 w-full disabled:opacity-50 disabled:cursor-not-allowed`}
+//       disabled={disabled}
+//       {...restProps}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+
+// export default MyButton;
+import React from 'react';
+
+
+const Mybutton = ({ text, bgColor = '#ff9500' }) => {
   return (
-    <button
+    <button 
       className={`bg-[${bgColor}] hover:brightness-110 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-0 w-full disabled:opacity-50 disabled:cursor-not-allowed`}
-      disabled={disabled}
-      {...restProps}
     >
-      {children}
+      {text}
     </button>
   );
 };
 
-export default MyButton;
+export default Mybutton;
+
