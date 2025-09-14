af485944b4e988727493883857f34a1756192786
Abdulrhman Rabea <abdorabee181@gmail.com>
2025-09-15 02:11:17 +0300

    Revert "footer and change in login page "

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,17 +1,15 @@
-// src/Layout.jsx
 import React from "react";
 import Nav from "./Navbar/Nav";
-import Home from "./pages/Home";      
+import Home from "./pages/Home";
 import { Outlet } from "react-router-dom";
-import Footer from "./Footer/Footer";     
 
 export default function Layout() {
-  return (
-    <>
-      <Nav />        
-      <Home />     
-      <Outlet />    
-      <Footer />    
-    </>
-  );
+	return (
+		<>
+			<Nav />
+			<Outlet></Outlet>
+			{/* others component */}
+			{/* <Footer/> */}
+		</>
+	);
 }
